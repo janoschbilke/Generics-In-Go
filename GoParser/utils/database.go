94b3ba5f9dcbf1f8cf89dab53cee47559b825eb6
Janosch Bilke <115155191+janoschbilke@users.noreply.github.com>
2026-04-20 11:46:54 +0200

    refactor main and outsource duplication (#16)

--- GoParser/utils/database.go
+++ GoParser/utils/database.go
@@ -1,9 +1,27 @@
 package utils
 
 import (
+	"GoParser/database"
 	"GoParser/model"
 	"reflect"
 )
+
+func CreateDatabase(config SetupConfiguration) (database.GenericsDatabase, error) {
+	var genericsDatabase database.GenericsDatabase
+	var err error
+
+	if config.OutputFormat == "csv" {
+		genericsDatabase, err = database.NewCsvDatabase("generic_counters.csv")
+	} else {
+		genericsDatabase, err = database.NewSQLiteDB("generic_counters.db")
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return genericsDatabase, nil
+}
 
 func GetColumns() []string {
 	var columns []string
